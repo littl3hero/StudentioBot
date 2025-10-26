4a4dc32c8c279bc9fda27e5ba6fd4e411854c531
littl3hero <freehandred@gmail.com>
2025-10-26 23:47:51 +0300

    Последняя стабильная версия

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,14 @@
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Sidebar } from '../components/Sidebar';
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <div className="min-h-screen bg-bg text-white flex">
+            <Sidebar />
+            <main className="flex-1 p-4 sm:p-6 lg:p-8">
+                <Component {...pageProps} />
+            </main>
+        </div>
+    );
+}
